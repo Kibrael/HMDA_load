fcc9f1580a7c7d2f196d1233b2a76d89026a5717
David Roell <breeroell@gmail.com>
2015-12-30 11:31:58 -0500

    adding create table logic for 1990 to 2004

--- load_scripts/create_spec.py
+++ load_scripts/create_spec.py
@@ -3,14 +3,9 @@
 from collections import OrderedDict
 import json
 
+#setup for building the JSON object
 field_list = OrderedDict({})
 end_points = {'start': 0, 'stop': 0}
-
-master_list_2007 ={'year': 4, 'rid': 10, 'agency':1, 'loan_type':1, 'loan_purpose':1, 'occupancy':1, 'amount':5, 'action':1,
-	'msa':5, 'state':2, 'county':3, 'tract':7, 'sex':1, 'co_sex':1, 'income':4, 'purchaser' :1, 'denial1' :1,
-	'denial2':1, 'denial3':1, 'edit_status':1, 'property_type':1, 'preapproval':1, 'ethnicity':1, 'co_ethnicity':1,
-	'race1':1, 'race2':1, 'race3':1, 'race4':1, 'race5':1, 'co_race1':1, 'co_race2':1, 'co_race3':1, 'co_race4':1, 'co_race5':1,
-	'rate_spread':5, 'HOEPA':1, 'lien':1, 'sequence':7}
 
 #lists of fields from FFIEC data specifications at the national archive
 fields_90_03 = ('year', 'rid', 'agency', 'loan_type', 'loan_purpose', 'occupancy', 'amount', 'action', 'msa', 'state', 'county',
@@ -22,10 +17,6 @@ fields_04_11 = ('year', 'rid', 'agency', 'loan_type', 'loan_purpose', 'occupancy
 	'rate_spread', 'hoepa', 'lien', 'sequence')
 
 #lengths of fields from FFIEC data specifications national archive
-lengths_2000 = ()
-lengths_2001 = ()
-lengths_2002 = ()
-lengths_2003 = ()
 lengths_90_03 = (4,10,1,1,1,1,5,1,4,2,3,7,1,1,1,1,4,1,1,1,1,1,7)
 lengths_04_11 = (4,10,1,1,1,1,5,1,5,2,3,7,1,1,4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,5,1,1,7)
 lengths_2012 = ()
@@ -75,3 +66,5 @@ write_spec('../specs/spec_2008.json', build_json(fields_04_11, lengths_04_11))
 write_spec('../specs/spec_2009.json', build_json(fields_04_11, lengths_04_11))
 write_spec('../specs/spec_2010.json', build_json(fields_04_11, lengths_04_11))
 write_spec('../specs/spec_2011.json', build_json(fields_04_11, lengths_04_11))
+
+#write file specs for 2011 to 2014
--- load_scripts/create_table.py
+++ load_scripts/create_table.py
@@ -18,44 +18,19 @@ def connect():
 		print "I am unable to connect to the database: ", e
 	return conn.cursor(), conn #returns connection and cursor
 
-def create_table_2007():
-
-	with open('../specs/spec_2007.json') as spec: #open the json file specification as a dictionary
-		spec_2007 = json.load(spec)
-
-	ordered_fields = ('year', 'rid', 'agency', 'loan_type', 'loan_purpose', 'occupancy', 'amount', 'action', 'msa', 'state', 'county',
-		'tract', 'sex', 'co_sex', 'income', 'purchaser', 'denial1', 'denial2', 'denial3', 'edit_status', 'property_type', 'preapproval',
-		'ethnicity', 'co_ethnicity', 'race1', 'race2', 'race3', 'race4', 'race5', 'co_race1', 'co_race2', 'co_race3', 'co_race4', 'co_race5',
-		'rate_spread', 'hoepa', 'lien', 'sequence')
-
-	table_attributes = []
-	for field in ordered_fields:
-		string = field + ' varchar(' + str(spec_2007[field]['length'])+ '), '
-		table_attributes.append(string)
-
-	create = '''DROP TABLE IF EXISTS HMDAPub2007; commit; CREATE TABLE HMDAPub2007 (''' + ' '.join(table_attributes)[:-2] + ') '
-
-	cur, conn = connect() #create cursor and connection objects
-	cur.execute(create,)
-	conn.commit() #commits the transaction
-
-def create_table(spec, table_name): #spec name must include folder path
+def create_table(spec, table_name, fields): #spec name must include folder path
 #this module should work for 2004-2008 LAR data
 	with open(spec) as spec:
 		file_spec = json.load(spec)
 
-	ordered_fields = ('year', 'rid', 'agency', 'loan_type', 'loan_purpose', 'occupancy', 'amount', 'action', 'msa', 'state', 'county',
-		'tract', 'sex', 'co_sex', 'income', 'purchaser', 'denial1', 'denial2', 'denial3', 'edit_status', 'property_type', 'preapproval',
-		'ethnicity', 'co_ethnicity', 'race1', 'race2', 'race3', 'race4', 'race5', 'co_race1', 'co_race2', 'co_race3', 'co_race4', 'co_race5',
-		'rate_spread', 'hoepa', 'lien', 'sequence')
-
 	table_attributes = []
-	for field in ordered_fields:
+	for field in fields:
 		string = field + ' varchar(' + str(file_spec[field]['length'])+ '), '
 		table_attributes.append(string)
 
 	create = 'DROP TABLE IF EXISTS ' + table_name + '; commit; CREATE TABLE ' + table_name + '(' + ' '.join(table_attributes)[:-2] + ') '
 	cur, conn = connect() #create cursor and connection objects
+
 	try:
 		cur.execute(create,)
 		conn.commit() #commits the transaction
@@ -65,9 +40,29 @@ def create_table(spec, table_name): #spec name must include folder path
 	cur.close()
 	conn.close()
 
+ordered_fields_04_11 = ('year', 'rid', 'agency', 'loan_type', 'loan_purpose', 'occupancy', 'amount', 'action', 'msa', 'state', 'county',
+		'tract', 'sex', 'co_sex', 'income', 'purchaser', 'denial1', 'denial2', 'denial3', 'edit_status', 'property_type', 'preapproval',
+		'ethnicity', 'co_ethnicity', 'race1', 'race2', 'race3', 'race4', 'race5', 'co_race1', 'co_race2', 'co_race3', 'co_race4', 'co_race5',
+		'rate_spread', 'hoepa', 'lien', 'sequence')
+
+ordered_fields_90_03 =  ('year', 'rid', 'agency', 'loan_type', 'loan_purpose', 'occupancy', 'amount', 'action', 'msa', 'state', 'county',
+	'tract', 'race', 'co_race', 'sex', 'co_sex', 'income', 'purchaser', 'denial1', 'denial2', 'denial3', 'edit_status', 'sequence')
+
 #function calls
-#create_table_2007()
+years_90_03 = [str(x+1990) for x in range(0,14)]
+years_04_11 = [str(x+2004) for x in range(0,8)]
+
 path = '../specs/'
-spec_name = 'spec_2004.json'
-table_name = 'HMDAPUB2004'
-create_table(path+spec_name, table_name)
+for year in years_04_11:
+	spec_name = path + 'spec_' + year+ '.json'
+	table_name = 'HMDAPub'+year
+	#create_table(spec_name, table_name, ordered_fields_04_11) #remove comment to run
+
+for year in years_90_03:
+	spec_name = path + 'spec_' + year + '.json'
+	table_name = 'HMDAPub' + year
+	#create_table(spec_name, table_name, ordered_fields_90_03) #remove comment to run
+
+
+
+
