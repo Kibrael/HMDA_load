4d44271450cb7b9489866d62c5c086cd41978065
David Roell <breeroell@gmail.com>
2016-01-18 11:12:58 -0500

    updating test files for 1991 spec

--- load_scripts/load_data.py
+++ load_scripts/load_data.py
@@ -19,28 +19,46 @@ def connect():
 		print "I am unable to connect to the database: ", e
 	return  conn, conn.cursor()#(cursor_factory=psycopg2.extras.DictCursor) #return a dictionary cursor object
 
-def load_rows(cur, conn, data, table):
+def load_rows_04_11(cur, conn, data, table, spec, fields):
 	count = 0
 	with open(data) as f:
 		rows = f.readlines()
-		for row in rows: #limited to 10 rows for testing
+		for row in rows:
 			print "loading row: ",count
-			parsed_row =  parse_row(row, spec_2007)
-			write_row(cur, conn, tuple(parsed_row), table)
+			parsed_row =  parse_row(row, spec)
+			write_row_04_11(cur, conn, tuple(parsed_row), table)
 			count +=1
 
-def parse_row(row, spec):
+def load_rows_90_03(cur, conn, data, table, spec, fields):
+	count = 0
+	with open(data) as f:
+		rows = f.readlines()
+		for row in rows:
+			print "loading row: ",count
+			parsed_row =  parse_row(row, spec, fields)
+			try:
+				write_row_90_03(cur, conn, tuple(parsed_row), table)
+				count +=1
+			except psycopg2.Error as e:
+				print "data load problem: ", e
+def parse_row(row, spec, fields):
 	delta_parsed = []
 	parsed = []
-	for key in ordered_fields:
+	for key in fields:
 		parsed.append(row[spec[key]['start']:spec[key]['stop']])
+		#if len(row[spec[key]['start']:spec[key]['stop']]) == 4:
+		#	print row[spec[key]['start']:spec[key]['stop']], type(row[spec[key]['start']:spec[key]['stop']]), len(row[spec[key]['start']:spec[key]['stop']])
 	for item in parsed:
 		item = "'"+ item + "'"
 		delta_parsed.append(item)
+
+	#print row
+	#print parsed
+	#print delta_parsed
 	return delta_parsed
 
-def write_row(cur, conn, row, table): #format should hold for 2004 to 2008
-	SQL = "INSERT INTO " + table + """(year, rid, agency, loan_type, loan_purpose, occupancy, amount,
+def write_row_04_11(cur, conn, row, table): #format should hold for 2004 to 2008
+	SQL = "INSERT INTO " + table + """ (year, rid, agency, loan_type, loan_purpose, occupancy, amount,
 		action, msa, state, county, tract, sex, co_sex, income, purchaser, denial1, denial2, denial3, edit_status, property_type,
 		preapproval, ethnicity, co_ethnicity, race1, race2, race3, race4, race5, co_race1, co_race2, co_race3, co_race4, co_race5,
 		rate_spread, hoepa, lien, sequence) VALUES ("""
@@ -49,24 +67,38 @@ def write_row(cur, conn, row, table): #format should hold for 2004 to 2008
 	SQL = SQL[:-2] + ');'
 	cur.execute(SQL,)
 
-#2007 field list, should hold for 2004 to 2008
-ordered_fields = ('year', 'rid', 'agency', 'loan_type', 'loan_purpose', 'occupancy', 'amount', 'action', 'msa', 'state', 'county',
+def write_row_90_03(cur, conn, row, table):
+	SQL = "SET CLIENT_ENCODING TO Latin1; INSERT INTO " + table + """ (year, rid, agency, loan_type, loan_purpose, occupancy, amount, action, msa, state, county,
+	tract, race, co_race, sex, co_sex, income, purchaser, denial1, denial2, denial3, edit_status, sequence) VALUES ("""
+
+	for value in row:
+		SQL = SQL + value + ', '
+	SQL = SQL[:-2] + ');'
+	#print SQL
+	cur.execute(SQL,)
+
+#field list for 2004 to 2011
+fields_04_11 = ('year', 'rid', 'agency', 'loan_type', 'loan_purpose', 'occupancy', 'amount', 'action', 'msa', 'state', 'county',
 	'tract', 'sex', 'co_sex', 'income', 'purchaser', 'denial1', 'denial2', 'denial3', 'edit_status', 'property_type', 'preapproval',
 	'ethnicity', 'co_ethnicity', 'race1', 'race2', 'race3', 'race4', 'race5', 'co_race1', 'co_race2', 'co_race3', 'co_race4', 'co_race5',
 	'rate_spread', 'hoepa', 'lien', 'sequence')
 
+fields_90_03 = ('year', 'rid', 'agency', 'loan_type', 'loan_purpose', 'occupancy', 'amount', 'action', 'msa', 'state', 'county',
+	'tract', 'race', 'co_race', 'sex', 'co_sex', 'income', 'purchaser', 'denial1', 'denial2', 'denial3', 'edit_status', 'sequence')
+
 
 #table and file variables - use config file to pull these from a json object
-table = "HMDAPUB2008"
-data_file = '../dat/HMDA2008.dat'
-spec_file = '../specs/spec_2008.json'
+table = "HMDAPUB1992"
+data_file = '../dat/HMDA1992.dat'
+spec_file = '../specs/spec_1991.json'
 
-with open(spec_file) as spec:
-	spec_2007 = json.load(spec) #load the json file specification
+with open(spec_file) as specification:
+	spec = json.load(specification) #load the json file specification
 
 
 conn, cur = connect() #connect to the locally hosted DB
-load_rows(cur, conn, data_file, table) #read the data file and insert rows
+#load_rows_04_11(cur, conn, data_file, table, spec, fields_04_11) #read the data file and insert rows for specified year (use for 2004 to 2011)
+load_rows_90_03(cur, conn, data_file, table, spec, fields_90_03) # read the data file and insert rows for the specifid year (use for 1990 to 2003)
 conn.commit() #commits the transaction
 cur.close()
 conn.close()
