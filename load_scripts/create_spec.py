4d5ed25168fbec307db36d176889c018a0734be1
David Roell <breeroell@gmail.com>
2016-01-22 10:07:03 -0500

    integrating config file with create_spec.py

--- load_scripts/create_spec.py
+++ load_scripts/create_spec.py
@@ -1,7 +1,22 @@
-#Creates a JSON output of the 2007 HMDA LAR file specification
-
+#Creates a JSON output for the selected years of the HMDA LAR file specification
 from collections import OrderedDict
 import json
+#write the file spec to a json file that includes field name, length, and start/stop positions
+def write_spec(name, data):
+	with open(name, 'w') as outfile: #writes the JSON structure to a file for the path named by report's header structure
+		json.dump(data, outfile, indent=4, ensure_ascii = False)
+
+#build the file spec as a dictionary that includes field names, length, and start/stop positions
+def build_json(fields, lengths):
+	for field in fields:
+		field_list[field] = {}
+	position = 0
+	for x in range(0,len(fields)):
+		field_list[fields[x]]['start'] = position
+		field_list[fields[x]]['stop'] = lengths[x] + position
+		field_list[fields[x]]['length'] = field_list[fields[x]]['stop']  - field_list[fields[x]]['start']
+		position += lengths[x]
+	return field_list
 
 #setup for building the JSON object
 field_list = OrderedDict({})
@@ -26,51 +41,34 @@ lengths_90_03 = (4,10,1,1,1,1,5,1,4,2,3,7,1,1,1,1,4,1,1,1,1,1,7)
 lengths_04_11 = (4,10,1,1,1,1,5,1,5,2,3,7,1,1,4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,5,1,1,7)
 lengths_12_14 = (4,10,1,1,1,1,1,5,1,1,5,2,3,7,1,1,1,1,1,1,1,1,1,1,1,1,1,1,4,1,1,1,1,5,1,1,1,7,8,6,8,6,8,8,1)
 
-#write the file spec to a json file that includes field name, length, and start/stop positions
-def write_spec(name, data):
-	with open(name, 'w') as outfile: #writes the JSON structure to a file for the path named by report's header structure
-		json.dump(data, outfile, indent=4, ensure_ascii = False)
 
-#build the file spec as a dictionary that includes field names, length, and start/stop positions
-def build_json(fields, lengths):
-	for field in fields:
-		field_list[field] = {}
-	position = 0
-	for x in range(0,len(fields)):
-		field_list[fields[x]]['start'] = position
-		field_list[fields[x]]['stop'] = lengths[x] + position
-		field_list[fields[x]]['length'] = field_list[fields[x]]['stop']  - field_list[fields[x]]['start']
-		position += lengths[x]
-	return field_list
+with open('config.json', 'r') as years_file:
+	years = json.load(years_file)
+
+years_90_03 = []
+years_04_11 = []
+years_12_14 = []
+path = '../specs/' #relative folder path to json file specs for parsing
+
+#build list of years for creating tables
+for year in years['load_years']:
+	if int(year) >= 1990 and int(year) <= 2003:
+		years_90_03.append(year)
+	elif int(year) >= 2004 and int(year) <= 2011:
+		years_04_11.append(year)
+	elif int(year) >= 2012 and int(year) <= 2014:
+		years_12_14.append(year)
+	else:
+		print "Invalid year selected"
 
 #write file specs for 1990 to 2003
-write_spec('../specs/spec_1990.json', build_json(fields_90_03, lengths_90_03))
-write_spec('../specs/spec_1991.json', build_json(fields_90_03, lengths_90_03))
-write_spec('../specs/spec_1992.json', build_json(fields_90_03, lengths_90_03))
-write_spec('../specs/spec_1993.json', build_json(fields_90_03, lengths_90_03))
-write_spec('../specs/spec_1994.json', build_json(fields_90_03, lengths_90_03))
-write_spec('../specs/spec_1995.json', build_json(fields_90_03, lengths_90_03))
-write_spec('../specs/spec_1996.json', build_json(fields_90_03, lengths_90_03))
-write_spec('../specs/spec_1997.json', build_json(fields_90_03, lengths_90_03))
-write_spec('../specs/spec_1998.json', build_json(fields_90_03, lengths_90_03))
-write_spec('../specs/spec_1999.json', build_json(fields_90_03, lengths_90_03))
-write_spec('../specs/spec_1990.json', build_json(fields_90_03, lengths_90_03))
-write_spec('../specs/spec_2000.json', build_json(fields_90_03, lengths_90_03))
-write_spec('../specs/spec_2001.json', build_json(fields_90_03, lengths_90_03))
-write_spec('../specs/spec_2002.json', build_json(fields_90_03, lengths_90_03))
-write_spec('../specs/spec_2003.json', build_json(fields_90_03, lengths_90_03))
+for year in years_90_03:
+	write_spec('../specs/spec_' + year + '.json', build_json(fields_90_03, lengths_90_03))
 
 #write file specs for 2004 to 2011
-write_spec('../specs/spec_2004.json', build_json(fields_04_11, lengths_04_11))
-write_spec('../specs/spec_2005.json', build_json(fields_04_11, lengths_04_11))
-write_spec('../specs/spec_2006.json', build_json(fields_04_11, lengths_04_11))
-write_spec('../specs/spec_2007.json', build_json(fields_04_11, lengths_04_11))
-write_spec('../specs/spec_2008.json', build_json(fields_04_11, lengths_04_11))
-write_spec('../specs/spec_2009.json', build_json(fields_04_11, lengths_04_11))
-write_spec('../specs/spec_2010.json', build_json(fields_04_11, lengths_04_11))
-write_spec('../specs/spec_2011.json', build_json(fields_04_11, lengths_04_11))
+for year in years_04_11:
+	write_spec('../specs/spec_' + year + '.json', build_json(fields_04_11, lengths_04_11))
 
 #write file specs for 2011 to 2014
-write_spec('../specs/spec_2012.json', build_json(fields_12_14, lengths_12_14))
-write_spec('../specs/spec_2013.json', build_json(fields_12_14, lengths_12_14))
-write_spec('../specs/spec_2014.json', build_json(fields_12_14, lengths_12_14))
+for year in years_12_14:
+	write_spec('../specs/spec_' + year + '.json', build_json(fields_12_14, lengths_12_14))
