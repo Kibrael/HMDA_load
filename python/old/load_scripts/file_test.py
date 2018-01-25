bb3bf111fc4800caae5a56389d4cba4d6cca605d
David Roell <breeroell@gmail.com>
2018-01-25 11:22:02 -0700

    remove old loading code

--- python/old/load_scripts/file_test.py
+++
@@ -1,7 +0,0 @@
-#creates a test file using the data range that threw errors on load
-
-with open('../dat/hmda1991.dat') as infile:
-	lines = infile.readlines()
-
-with open('../dat/hmda1991_slice.dat', 'w') as outfile:
-	outfile.write(''.join(lines[3226037:3227000]))
--- python/old/load_scripts/testing.py
+++
@@ -1,9 +0,0 @@
-#prints the characters responsible for throwing errors.
-#Theory: null values were used instead of NAs and populate as ASCII 0s. These need to be replaced with a string length 4, likely 'NA  '
-
-with open('../dat/hmda1991_slice.dat') as f:
-    lines = f.readlines()
-
-for line in lines[4:5]:
-    for char in line:
-        print ord(char)
